536451f69fa4f6d8db3c8135e611b245052db37c
Kristofer Rye <kristofer.rye@gmail.com>
2020-01-19 11:23:36 -0600

    Add Sphere type
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod intersect;
 mod products;
 mod ray;
+mod sphere;
 mod triangle;
 mod vec3;
 
 pub use intersect::{Hit, Intersect};
 pub use products::{CrossProduct, DotProduct};
 pub use ray::Ray;
+pub use sphere::Sphere;
 pub use triangle::Triangle;
 pub use vec3::Vec3;
---
+++ src/sphere.rs
@@ -0,0 +1,6 @@
+use crate::vec3::Vec3;
+
+pub struct Sphere {
+	pub(crate) origin: Vec3,
+	pub(crate) radius: f32,
+}
